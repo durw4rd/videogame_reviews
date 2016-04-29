261151e76baceca0217fa88a64fce28b5d41b4c7
durw4rd <misa.fasa@gmail.com>
2016-04-29 17:46:57 +0200

    add exception to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 class VideogameReviews::Application
   config.middleware.insert_before 0,  "Rack::Cors", debug: true, logger: (-> { Rails.logger }) do
     allow do
-      origins 'localhost:3000', '127.0.0.1:3000'
+      origins 'localhost:3000', '127.0.0.1:3000', 'welder-polecat-20526.bitballoon.com'
 
       resource '/cors',
         headers: :any,
